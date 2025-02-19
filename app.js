fe5748867c01827b69e1e5f56619f370825783f0
mohtishim <mohtishim5@gmail.com>
2025-02-20 00:59:04 +0500

    1st comp

---
+++ app.js
@@ -0,0 +1,51 @@
+// Chapter 10 (if statements)
+
+// Q1
+// var city = "karachi";
+// if (city == "karachi"){
+//     console.log("The city of lights")
+// }
+
+// Q2
+
+// var x = 1;
+// var y = 1;
+
+// if (x === y){
+//     var z = +prompt("Enter number")
+// }
+
+// console.log(`The value of z is: ${z}`)
+
+// Q3
+// var zip = +prompt ("Enter zip code");
+// if (zip === 10010){
+//     alert("Karachi");
+// }
+// else{
+//     alert("Enter correct city");
+// }
+
+// Q4
+// var x = 1;
+// if (x===1){
+//     x=25;
+// }
+// console.log(x);
+
+
+
+// Chapter 11 (Comparison Operators)
+
+// Q1
+
+// var x = 10;
+// var y = 20;
+// if (x != y){
+//     alert("Unequal");
+// }
+
+// Q2
+var x = 10;
+var y = 20;
+
