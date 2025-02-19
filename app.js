ce3f30d2acd7c3c7b23e842ffac705919c76a3fc
mohtishim <mohtishim5@gmail.com>
2025-02-20 01:43:35 +0500

    2nd comp

--- app.js
+++ app.js
@@ -46,6 +46,80 @@
 // }
 
 // Q2
-var x = 10;
-var y = 20;
+// var x = 30;
+// var y = 20;
 
+// if (x>=y){
+//     alert("X is greater than or equal to Y")
+// }
+
+// Q3
+// var x = 1;
+// var y = 2;
+// if (x!==y){
+//     x=2;
+//     console.log(x);
+// }
+
+// Q4
+// var x = 1;
+// var y = 2;
+// if (x!==y){
+//     alert("Congo pongo");
+// }
+
+// Q5
+// var firstName = prompt ("Enter name");
+// if (firstName.toLowerCase() == "mohtishim"){
+//     alert("No Match");
+// }
+
+// Chapter 12 (if…else and else if statements)
+
+// Q1
+// var x = 2;
+// var y = 3;
+// if (x===y){
+//     alert("X is equal");
+// }
+// else if (x != y){
+//     alert("X is unequal to Y");
+// }
+
+// Q2
+
+// var chemMarks = +prompt("Enter Chemitry marks");
+// var phyMarks = +prompt("Enter Physics marks");
+// var bioMarks = +prompt("Enter Biology marks");
+
+// var totalMarks = chemMarks+phyMarks+bioMarks
+
+// if (totalMarks>=150){
+//     alert("You Are Pass");
+// }
+
+// else if(totalMarks<=149){
+//     alert("You Are Fail");
+// }
+
+// Q3
+// var a = +prompt("Enter value of a");
+// if (a===10){
+//     alert("a = 10");
+// }
+// else if (a != 10){
+//     alert(`a = ${a}`);
+// }
+
+// Q4
+// var city = prompt("Enter city");
+// if (city.toLowerCase() == "karachi"){
+//     alert("Welcome from karachi");
+// }
+
+// else if (city.toLowerCase() == "lahore"){
+//     alert("Welcome from karachi");
+// }
+// else{
+//     alert("Unknown city");
+// }
